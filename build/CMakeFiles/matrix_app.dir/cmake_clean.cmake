170916b98a4e18a38fb9ca2e77d114b08de6a608
sakil <sakilxotwod@gmail.com>
2026-03-25 21:13:06 +0530

    files

---
+++ build/CMakeFiles/matrix_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_app.dir/src/main.cpp.o"
+  "CMakeFiles/matrix_app.dir/src/main.cpp.o.d"
+  "CMakeFiles/matrix_app.dir/src/matrix.cpp.o"
+  "CMakeFiles/matrix_app.dir/src/matrix.cpp.o.d"
+  "matrix_app"
+  "matrix_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
